a9156f1377d66e52360b757117d3a8b30302335c
Ritik Raj <ritikraj1875@gmail.com>
2024-08-22 16:57:09 +0530

    v1

--- Smart Garbage Management/src/App.jsx
+++ Smart Garbage Management/src/App.jsx
@@ -3,12 +3,14 @@ import Signup from './Components/Signup.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './Components/Login.jsx';
+import Landing from './landing_Page/landing.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Use the `element` prop instead of `component` */}
+        <Route path = "/" element = {Landing}/>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element = {<Login/>}/>
       </Routes>
