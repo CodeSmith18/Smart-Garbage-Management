c74fa0242e4abf490808a2f2d932684e38588c78
Deepam <deepamkgupta@gmail.com>
2024-08-22 16:16:58 +0530

    a

---
+++ Smart Garbage Management/src/landing_Page/landing.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import i1 from "../assets/images/landing.png";
+function Landing() {
+	return (
+		<div>
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<img src={i1} alt="" />
+		</div>
+	);
+}
+export default Landing;
